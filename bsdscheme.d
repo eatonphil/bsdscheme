87dd5aba1f359ae47eeee2d754e02e1f0cc102bb
Phil Eaton <phil@Rebeccas-MacBook-Pro.local>
2017-10-11 22:20:14 -0400

    Let statements working

--- bsdscheme.d
+++ bsdscheme.d
@@ -1,6 +1,8 @@
 import std.conv;
 import std.stdio;
 import std.string;
+import std.typecons;
+import std.variant;
 
 enum TokenType {
   LeftParen,
@@ -22,7 +24,7 @@ class Buffer(T) {
 
   this(T[] buffer) {
     this.buffer = buffer;
-    this.index = -1;
+    this.index = 0;
   }
 
   this() {
@@ -89,17 +91,25 @@ Token* lexRightParen(StringBuffer input) {
 
 Token* lexSymbol(StringBuffer input) {
   string symbol = "";
-  string currentCharAsString = "";
 
-  while ((currentCharAsString = to!string(input.current())) != " ") {
-    symbol ~= currentCharAsString;
+ loop: do {
+    auto c = input.current();
 
-    if (!input.next()) {
+    switch (c) {
+    case '(':
+    case ')':
+    case ' ':
+    case '\n':
+    case '\t':
+      break loop;
       break;
+    default:
+      symbol ~= to!string(c);
     }
-  }
+  } while (input.next());
 
   if (symbol.length) {
+    input.previous();
     return new Token(0, 0, "", symbol, TokenType.Symbol);
   }
 
@@ -128,78 +138,197 @@ TokenBuffer lex(StringBuffer input) {
     if (token !is null) {
       tokens.push(token);
     }
-  } while (!input.next());
+  } while (input.next());
 
   return tokens;
 }
 
 alias Buffer!(SExp*) SExpBuffer;
+alias Token Atom;
 
-struct Exp {
-  SExp* head;
-  SExpBuffer tail;
-};
+struct SExp {
+  Atom* atom;
+  SExp*[] sexps;
+}
 
-alias Atom Token;
+Tuple!(Token*[], SExp*) parse(Token*[] tokens, SExp* sexp) {
+  int i = 0;
 
-class SExp {
-  union {
-    Exp* exp;
-    Atom* atom;
-  } value;
+  while (true) {
+    if (i == tokens.length || tokens[i] is null) {
+      break;
+    }
 
-  bool isAtom() {
-    return this.value.atom !is null;
+    auto token = tokens[i];
+
+    switch (token.type) {
+    case TokenType.LeftParen:
+      auto program = parse(tokens[i + 1 .. tokens.length]);
+      (*sexp).sexps ~= program[1];
+      tokens = program[0];
+      i = -1;
+      break;
+    case TokenType.RightParen:
+      return Tuple!(Token*[], SExp*)(tokens[i + 1 .. tokens.length], sexp);
+      break;
+    default:
+      auto atom = new SExp;
+      atom.atom = token;
+      (*sexp).sexps ~= atom;
+      break;
+    }
+
+    i += 1;
   }
-};
 
-class Program {
-  SExp* program;
+  return Tuple!(Token*[], SExp*)(tokens[i + 1 .. tokens.length], sexp);
+}
 
-  this(SExp program) {
-    this.program = program;
+Tuple!(Token*[], SExp*) parse(Token*[] tokens) {
+  return parse(tokens, new SExp);
+}
+
+void print(SExp* sexp) {
+  if (sexp is null) {
+    return;
   }
 
-  void run() {
-    
+  if (sexp.atom !is null) {
+    return;
+  }
+
+  if (sexp.sexps !is null) {
+    foreach (ref _sexp; sexp.sexps) {
+      print(_sexp);
+    }
   }
 }
 
-Program parse(TokenBuffer tokens) {
-  SExp* sexp;
-  SExp* currentSexp = sexp;
+struct Value {
+  int* _integer;
+  string* _string;
+  string* _symbol;
+  bool _nil;
+  Value* function(SExp*[], Context ctx) _fun;
+}
 
-  do {
-    auto token = tokens.current();
+Value* plus(SExp*[] arguments, Context ctx) {
+  int i = 0;
 
-    if (token.type == LeftParen) {
-      if (currentSexp is null) {
-        currentSexp = new SExp;
-      } else if (current.head is null) {
-        currentSexp.head = new SExp;
-        currentSexp = sexp.head;
+  foreach (arg; arguments) {
+    i += *(*(interpret(arg, ctx)))._integer;
+  }
+
+  auto v = new Value;
+  v._integer = new int(i);
+  return v;
+}
+
+Value* minus(SExp*[] arguments, Context ctx) {
+  int i = *(*(interpret(arguments[0], ctx)))._integer;
+
+  foreach (arg; arguments[1 .. arguments.length]) {
+    i -= *(*(interpret(arg, ctx)))._integer;
+  }
+
+  auto v = new Value;
+  v._integer = new int(i);
+  return v;
+}
+
+Value* let(SExp*[] arguments, Context ctx) {
+  auto bindings = arguments[0].sexps;
+  auto letBody = arguments[1];
+
+  Context newCtx = ctx.dup();
+
+  foreach (binding; bindings) {
+    auto key = binding.sexps[0].atom.value;
+    auto value = binding.sexps[1];
+    newCtx.set(key, interpret(value, ctx));
+  }
+
+  return interpret(letBody, newCtx);
+}
+
+class Context {
+  Value*[string] map;
+
+  void set(string key, Value* value) {
+    this.map[key] = value;
+  }
+
+  Context dup() {
+    Context dup = new Context;
+    dup.map = this.map.dup;
+    return dup;
+  }
+
+  Value* get(string key) {
+    Value* value = null;
+
+    if (key in this.map) {
+      value = this.map[key];
+    }
+
+    if (isNumeric(key)) {
+      value = new Value;
+      value._integer = new int(to!int(key));
+    } else if (key == "+") {
+      value = new Value;
+      value._fun = &plus;
+    } else if (key == "-") {
+      value = new Value;
+      value._fun = &minus;
+    } else if (key == "let") {
+      value = new Value;
+      value._fun = &let;
+    }
+
+    return value;
+  }
+}
+
+Value* interpret(SExp* sexp, Context ctx) {
+  Value* v = new Value;
+
+  if (sexp is null) {
+    // TODO: handle this?
+    v._nil = true;
+    return v;
+  }
+
+  if (sexp.atom !is null) {
+    return ctx.get(sexp.atom.value);
+  }
+
+  if (sexp.sexps !is null) {
+    auto head = interpret(sexp.sexps[0], ctx);
+    auto tail = sexp.sexps[1 .. sexp.sexps.length];
+
+    if (!head._nil) {
+      if (head._fun !is null) {
+        return (*(head._fun))(tail, ctx);
+      } else if (tail.length == 0) {
+        return head;
       } else {
-        sexp.tail.push(new SExp);
-        sexp.tail.next();
-        currentSexp = sexp.tail.current();
-      }
-    } else if (token.type == RightParen) {
-      if (sexp.head !is null) {
-        sexp.tail.increase(1);
-        sexp.tail.next();
+        // TODO: handle head not being a function
       }
     } else {
-      currentSexp.value.atom = token;
+      // TODO: handle this: ((identity +) 1 2)
     }
-  } while (!tokens.next());
+  }
 
-  return new Program(sexp);
+  v._nil = true;
+  return v;
+  // TODO: handle this?
 }
 
 int main() {
-  auto source = "(+ (- 3 2) 5)".dup;
+  auto source = "(let ((a 7)) (+ (- 3 2) a 3))".dup;
   auto tokens = lex(new StringBuffer(source));
-  auto program = parse(tokens);
-  program.run();
+  auto program = parse(tokens.buffer)[1];
+  auto value = interpret(program, new Context);
+  writeln(*value._integer);
   return 0;
 }
